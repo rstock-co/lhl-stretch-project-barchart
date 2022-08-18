e9641a724672715b293bdb3acbf5a4bf8054b706
Richard Stock <rstock.co@gmail.com>
2022-08-18 16:33:50 -0600

    update font sizing and coloring

--- js/data.js
+++ js/data.js
@@ -5,15 +5,15 @@ const Chart = {
   options: {
     chartWidth: 600,
     chartHeight: 300,
-    labelsPosition: 'top',      // can be top, bottom, or middle of the bar
-    barWidth: 100,              // needs to be calculated based on total amount of values
-    barHeight: 900,             // needs to be calculated based on the values themselves
-    barColor: '#ffd100',        // needs to be customizable (also for each label if stacked bar chart)
-    labelColor: 'black',        // needs to be customizable (also for each label if stacked bar chart)
-    barSpacing: 1,              // needs to be customizable
-    numGridlines: 5,            // needs to be either customizable or calculated based on the values themselves
-    chartTitleFontSize: 16,     // needs to set and shown dynamically, and customizable
-    chartTitleFontColor: 'blue' // needs to set and shown dynamically, and customizable
+    labelsPosition: 'top',            // can be top, bottom, or middle of the bar
+    barWidth: 100,                    // needs to be calculated based on total amount of values
+    barHeight: 900,                   // needs to be calculated based on the values themselves
+    barColor: '#f4a261',   // needs to be customizable (also for each label if stacked bar chart)
+    labelColor: '#C50A1A',  // needs to be customizable (also for each label if stacked bar chart)
+    barSpacing: 1,                    // needs to be customizable
+    numGridlines: 5,                  // needs to be either customizable or calculated based on the values themselves
+    chartTitleFontSize: 16,           // needs to set and shown dynamically, and customizable
+    chartTitleFontColor: 'blue'       // needs to set and shown dynamically, and customizable
   },
   element: 'element goes here',
    // the DOM or jQuery element where the chart will be rendered into

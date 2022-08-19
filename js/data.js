e0fdf576b563bd23a098a0140c2085ed5556f594
Richard Stock <rstock.co@gmail.com>
2022-08-19 14:38:57 -0600

    reposition current chart values display

--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 // Define the data structure / requirements for the bar chart, based on project specifications
 
 const Chart = {
-  values: [['CAL',54],['VAN',96],['TOR',132],['WAS',77],['NYC',236],['SEA',83],['EDM',77]],           // need to also allow user to pass multiple values for each bar if stacked chart (array of arrays)
+  values: [['Calgary',54],['Victoria',96],['Toronto',132],['Regina',77],['Portland',236],['Seattle',83],['Ottowa',167]],           // need to also allow user to pass multiple values for each bar if stacked chart (array of arrays)
   options: {
     chartWidth: 600,
     chartHeight: 300,
@@ -13,7 +13,7 @@ const Chart = {
     barSpacing: 1,                    // needs to be customizable
     numGridlines: 5,                  // needs to be either customizable or calculated based on the values themselves
     chartTitleFontSize: 16,           // needs to set and shown dynamically, and customizable
-    chartTitleFontColor: 'blue'       // needs to set and shown dynamically, and customizable
+    chartTitleFontColor: 'blue'       // needs to set and shown dynamically, and customizableLAC 34
   },
   element: 'element goes here',
    // the DOM or jQuery element where the chart will be rendered into
